5cc3b7cedc72d51d3bdbea1a1f69c3e6d937e75e
Tymofii212 <tymofiimoysey@gmail.com>
2024-10-26 09:51:48 -0500

    Innit comit

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ class Employee:
         self.Name = name
         self.Age = age
         self.Experience = experience
-        self.Salary= experience*400+8000
+        self.Rate = 400
+        self.Salary= self.Experience * self.Rate + 8000
 
     def print_employee_info(self):
         print("Прізвище:", self.Surname)
@@ -25,8 +26,9 @@ emloyee1.to_receive_a_salary()
 class Driver(Employee):
     def __init__(self,surname, name, age, experience,transport):
         super().__init__(surname,name,age,experience)
+        self.Rate=1100
         self.Transport=transport
-        self.TSalary = experience * 1005 + 8000
+        self.TSalary = experience * 1000 + 8000
     def show_transport(self):
         print("Транспорт:", self.Transport)
     def to_receive_a_salary(self):
@@ -40,3 +42,24 @@ emloyee2=Driver("Kogut","Oleg",27,2,"AboBus Bohdan")
 emloyee2.print_employee_info()
 emloyee2.show_transport()
 emloyee2.to_receive_a_salary()
+
+class Sigmector(Employee):
+    def __init__(self,surname, name, age, experience,licence):
+        super().__init__(surname,name,age,experience)
+        self.Rate=1100
+        self.Licence=licence
+        self.Lsalary = experience * 1000 + 8000
+
+    def show_licence(self):
+        print("Ліцензія:", self.Licence)
+    def to_receive_a_salary(self):
+        print("Ваша Зарплата:", self.LSalary)
+
+print()
+try:
+    emloyee3=Sigmector("Skala","Mychailo",15,1,"BO4184821CI")
+    emloyee3.print_employee_info()
+    emloyee3.show_licence()
+    emloyee3.to_receive_a_salary()
+except TypeError as error:
+    print(f"Error:{error}")
